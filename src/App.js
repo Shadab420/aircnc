156e34acd5b002b33945d2dfdec2a8f28b9915f1
Shadab420 <shadab_anwar@outlook.com>
2020-06-04 19:37:24 +0600

    installed necessary packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGa from 'react-ga';
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 function App() {
+  // useEffect(() => {
+	
+	// 	ReactGa.initialize('tracking-code')
+		
+	// 	//to report pageview
+	// 	ReactGa.pageview(window.location.pathname + window.location.search)
+	
+  // }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+      
     </div>
   );
 }
