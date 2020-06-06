f9b1d94fcfa78a50198c10f1134e74e12ed6aee5
Shadab420 <shadab_anwar@outlook.com>
2020-06-06 17:10:55 +0600

    map integrated

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
+import MapGL from 'react-map-gl';
+require('dotenv').config();
 
 const Booking = (props) => {
 
     const {searchInfo} = props;
+    const [viewport, setViewport] = useState({
+        latitude: 23.808217,
+		longitude: 90.411655,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0
+      });
+    
     
     return (
         <div>
@@ -13,7 +23,15 @@ const Booking = (props) => {
                     
                 </Col>
                 <Col lg={6} md={6} sm={12} xs={12}>
-                    <h3>Map</h3>
+                    <MapGL
+                        {...viewport}
+                        width="100%"
+                        height="100vh"
+                        mapStyle="mapbox://styles/mapbox/streets-v11"
+                        onViewportChange={nextViewport => setViewport(nextViewport)}
+                        mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
+                    />
+                    
                 </Col>
             </Row>
         </div>
